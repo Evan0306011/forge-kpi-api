5314041bc4fc7ebec43e034a5250bd8e66cef3a8
Evan0306011 <67109934+Evan0306011@users.noreply.github.com>
2026-09-18 15:57:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from mcp.server.fastmcp import FastMCP
 
 app = FastAPI()
 
-mcp = FastMCP("Forge KPI MCP")
-
 @app.get("/")
 def root():
-    return {"status":"working-v3"}
+    return {"status":"working-v4"}
 
 @app.get("/routes")
 def routes():
