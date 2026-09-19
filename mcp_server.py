fdd256cc3ab1cae307837ca1aad9fe7b0618560d
Evan0306011 <67109934+Evan0306011@users.noreply.github.com>
2026-09-19 18:04:48 +1000

    Update mcp_server.py

--- mcp_server.py
+++ mcp_server.py
@@ -1,22 +1,15 @@
 from mcp.server.mcpserver import MCPServer
 
-# Create MCP Server
 mcp = MCPServer("RF Analysis Expert")
 
-# Test Tool
 @mcp.tool()
 def hello():
-    """
-    Test MCP tool.
-    """
+    """Test tool"""
     return "Hello"
 
-# Start MCP Server
 if __name__ == "__main__":
     mcp.run(
         transport="streamable-http",
         host="0.0.0.0",
-        port=8080,
-        streamable_http_path="/mcp"
+        port=8080
     )
-`
